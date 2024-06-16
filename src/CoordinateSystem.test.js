7cbb5c778a449952e3917104555774e37e611ed1
Philip Johnson <p.zhao.johnson@gmail.com>
2024-06-16 17:36:35 -0400

    Add functionality for converting to and from the client coordinate system

--- src/CoordinateSystem.test.js
+++ src/CoordinateSystem.test.js
@@ -48,4 +48,102 @@ describe('CoordinateSystem class', () => {
 
     expect(coordinateSystem.maxY).toBe(32.98 + 202.184);
   });
+
+  test('clientWidth getter', () => {
+    let targetSVGDoc = { getBoundingClientRect: () => ({ width: 560.27 }) };
+
+    let coordinateSystem = new CoordinateSystem(targetSVGDoc);
+
+    expect(coordinateSystem.clientWidth).toBe(560.27);
+  });
+
+  test('clientHeight getter', () => {
+    let targetSVGDoc = { getBoundingClientRect: () => ({ height: 873.225 }) };
+
+    let coordinateSystem = new CoordinateSystem(targetSVGDoc);
+
+    expect(coordinateSystem.clientHeight).toBe(873.225);
+  });
+
+  test('horizontalClientScaling getter', () => {
+    let targetSVGDoc = {
+      viewBox: { baseVal: { width: 809.043 } },
+      getBoundingClientRect: () => ({ width: 1120.37 }),
+    };
+
+    let coordinateSystem = new CoordinateSystem(targetSVGDoc);
+
+    expect(coordinateSystem.horizontalClientScaling).toBeCloseTo(1120.37 / 809.043);
+  });
+
+  test('verticalClientScaling getter', () => {
+    let targetSVGDoc = {
+      viewBox: { baseVal: { height: 2027.84 } },
+      getBoundingClientRect: () => ({ height: 1382.91 }),
+    };
+
+    let coordinateSystem = new CoordinateSystem(targetSVGDoc);
+
+    expect(coordinateSystem.verticalClientScaling).toBeCloseTo(1382.91 / 2027.84);
+  });
+
+  test('minClientX getter', () => {
+    let targetSVGDoc = { getBoundingClientRect: () => ({ x: -28.4992 }) };
+
+    let coordinateSystem = new CoordinateSystem(targetSVGDoc);
+
+    expect(coordinateSystem.minClientX).toBe(-28.4992);
+  });
+
+  test('minClientY getter', () => {
+    let targetSVGDoc = { getBoundingClientRect: () => ({ y: 59.832 }) };
+
+    let coordinateSystem = new CoordinateSystem(targetSVGDoc);
+
+    expect(coordinateSystem.minClientY).toBe(59.832);
+  });
+
+  test('fromClientX method', () => {
+    let targetSVGDoc = {
+      viewBox: { baseVal: { width: 2388.72 } },
+      getBoundingClientRect: () => ({ x: 328.991, width: 1480.2 }),
+    };
+
+    let coordinateSystem = new CoordinateSystem(targetSVGDoc);
+
+    expect(coordinateSystem.fromClientX(-80.3)).toBeCloseTo(((-80.3) - 328.991) / (1480.2 / 2388.72));
+  });
+
+  test('fromClientY method', () => {
+    let targetSVGDoc = {
+      viewBox: { baseVal: { height: 670.1 } },
+      getBoundingClientRect: () => ({ y: -203.58, height: 1100.88 }),
+    };
+
+    let coordinateSystem = new CoordinateSystem(targetSVGDoc);
+
+    expect(coordinateSystem.fromClientY(18.4)).toBeCloseTo((18.4 - (-203.58)) / (1100.88 / 670.1));
+  });
+
+  test('toClientX method', () => {
+    let targetSVGDoc = {
+      viewBox: { baseVal: { x: -28.18, width: 901.37 } },
+      getBoundingClientRect: () => ({ width: 1503.2 }),
+    };
+
+    let coordinateSystem = new CoordinateSystem(targetSVGDoc);
+
+    expect(coordinateSystem.toClientX(250.4)).toBeCloseTo((1503.2 / 901.37) * (250.4 - (-28.18)));
+  });
+
+  test('toClientY method', () => {
+    let targetSVGDoc = {
+      viewBox: { baseVal: { y: 82.5, height: 1533 } },
+      getBoundingClientRect: () => ({ height: 1204.5 }),
+    };
+
+    let coordinateSystem = new CoordinateSystem(targetSVGDoc);
+
+    expect(coordinateSystem.toClientY(-501)).toBeCloseTo((1204.5 / 1533) * ((-501) - 82.5));
+  });
 });
--- src/CoordinateSystem.ts
+++ src/CoordinateSystem.ts
@@ -1,3 +1,8 @@
+type Point = {
+  x: number;
+  y: number;
+};
+
 /**
  * Represents the coordinate system of a target SVG document.
  */
@@ -46,5 +51,149 @@ export class CoordinateSystem {
    */
   get maxY(): number {
     return this.minY + this.height;
+  }
+
+  /**
+   * The width of the bounding client rect for the target SVG document.
+   */
+  get clientWidth(): number {
+    return this.targetSVGDoc.getBoundingClientRect().width;
+  }
+
+  /**
+   * The height of the bounding client rect for the target SVG document.
+   */
+  get clientHeight(): number {
+    return this.targetSVGDoc.getBoundingClientRect().height;
+  }
+
+  /**
+   * The horizontal scaling factor going from the coordinate system of the target SVG document
+   * (i.e., as defined by its view box)
+   * to the client coordinate system
+   * (i.e., the coordinate system used by methods such as `getBoundingClientRect`).
+   */
+  get horizontalClientScaling(): number {
+    return this.clientWidth / this.width;
+  }
+
+  /**
+   * The vertical scaling factor going from the coordinate system of the target SVG document
+   * (i.e., as defined by its view box)
+   * to the client coordinate system
+   * (i.e., the coordinate system used by methods such as `getBoundingClientRect`).
+   */
+  get verticalClientScaling(): number {
+    return this.clientHeight / this.height;
+  }
+
+  /**
+   * The minimum X coordinate of the bounding client rect for the target SVG document.
+   */
+  get minClientX(): number {
+    return this.targetSVGDoc.getBoundingClientRect().x;
+  }
+
+  /**
+   * The minimum Y coordinate of the bounding client rect for the target SVG document.
+   */
+  get minClientY(): number {
+    return this.targetSVGDoc.getBoundingClientRect().y;
+  }
+
+  /**
+   * Converts an X coordinate in the client coordinate system
+   * (i.e., the coordinate system used by methods such as `getBoundingClientRect`)
+   * to the coordinate system of the target SVG document
+   * (i.e., as defined by its view box).
+   */
+  fromClientX(clientX: number): number {
+    return (clientX - this.minClientX) / this.horizontalClientScaling;
+  }
+
+  /**
+   * Converts a Y coordinate in the client coordinate system
+   * (i.e., the coordinate system used by methods such as `getBoundingClientRect`)
+   * to the coordinate system of the target SVG document
+   * (i.e., as defined by its view box).
+   */
+  fromClientY(clientY: number): number {
+    return (clientY - this.minClientY) / this.verticalClientScaling;
+  }
+
+  /**
+   * Converts a pair of X and Y coordinates in the client coordinate system
+   * (i.e., the coordinate system used by methods such as `getBoundingClientRect`)
+   * to the coordinate system of the target SVG document
+   * (i.e., as defined by its view box).
+   *
+   * @returns A tuple of X and Y coordinates.
+   */
+  fromClientCoordinates(clientX: number, clientY: number): [number, number] {
+    return [
+      this.fromClientX(clientX),
+      this.fromClientY(clientY),
+    ];
+  }
+
+  /**
+   * Converts a point in the client coordinate system
+   * (i.e., the coordinate system used by methods such as `getBoundingClientRect`)
+   * to the coordinate system of the target SVG document
+   * (i.e., as defined by its view box).
+   */
+  fromClientPoint(clientPoint: Point): Point {
+    return {
+      x: this.fromClientX(clientPoint.x),
+      y: this.fromClientY(clientPoint.y),
+    };
+  }
+
+  /**
+   * Converts an X coordinate in the coordinate system of the target SVG document
+   * (i.e., as defined by its view box)
+   * to the client coordinate system
+   * (i.e., the coordinate system used by methods such as `getBoundingClientRect`).
+   */
+  toClientX(x: number): number {
+    return this.horizontalClientScaling * (x - this.minX);
+  }
+
+  /**
+   * Converts a Y coordinate in the coordinate system of the target SVG document
+   * (i.e., as defined by its view box)
+   * to the client coordinate system
+   * (i.e., the coordinate system used by methods such as `getBoundingClientRect`).
+   */
+  toClientY(y: number): number {
+    return this.verticalClientScaling * (y - this.minY);
+  }
+
+  /**
+   * Converts a pair of X and Y coordinates in the coordinate system of the target SVG document
+   * (i.e., as defined by its view box)
+   * to the client coordinate system
+   * (i.e., the coordinate system used by methods such as `getBoundingClientRect`).
+   *
+   * @returns A tuple of client X and Y coordinates.
+   */
+  toClientCoordinates(x: number, y: number): [number, number] {
+    return [
+      this.toClientX(x),
+      this.toClientY(y),
+    ];
+  }
+
+  /**
+   * Converts a point in the coordinate system of the target SVG document
+   * (i.e., as defined by its view box)
+   * to the client coordinate system
+   * (i.e., the coordinate system used by methods such as `getBoundingClientRect`).
+   */
+  toClientPoint(point: Point): Point {
+    return {
+      x: this.toClientX(point.x),
+      y: this.toClientY(point.y),
+    };
   }
 }
